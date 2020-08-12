db7a54636ba5588fb4790ec82716572a19620009
Eder Cirino <edersantoscirino@gmail.com>
2020-08-12 18:49:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+## InviteGuest - An Web application to manage events
+
+### Technologies used:
+
+- HTML5
+- Bootstrap 4.5
+- Javascript(ES6)
+
+------------
+This project was developed without javascript framework. The biggest challenge faced in this project was the client-side interaction through javascript.
+
+***PS: All registers are going be storage in the browser(localStorage).***
+
+#### How it works
+
+The user(receptionist in the most cases) type the host name and eeceptionist name(him/her own name). After this, the section will showed to add the guests.
+
+
+#### Future implementations
+
+- Order the list
+- Implement input age
+- Search bar to filter the list
+
+------------
+### How to test this project in your computer
+
+#### prerequisites:
+1. Yarn installed - https://yarnpkg.com/
+2. A text editor (Like [VSCode](https://code.visualstudio.com/ "VSCode"))
+
+------------
+##### Hands on!
+- Clone the repository
+- run the command `yarn install`
+- install bootstrap dependencies running the command `yarn add boostrap@4.5.0`
+- install popperjs and jquery dependencies with the command `yarn add jquery e yarn add popper.js`
+- Install sass `yarn add sass`
+- Compile the css files with the command `sass --watch node_modules/bootstrap/scss:node_modules/bootstrap/compiler style/scss:node_modules/bootstrap/compiler`
+- run the command `yarn dev`
+- Execute the index.html file 
+
+------------
---
+++ src/index.js
@@ -0,0 +1,189 @@
+class Host {
+
+    constructor(){
+        this.hostName;
+    }
+
+    create(hostName){
+
+        if (hostName == "") {
+            alert("Digite um nome.");
+        }else{   
+            localStorage.setItem("hostName", JSON.stringify(hostName));
+            document.getElementById("btnAddHostName").style.display = "none";
+            document.getElementById("hostNameInput").value = "";
+            document.getElementById("btnDelHostName").style.display = "block";
+            window.location.reload();
+
+        }
+
+    }
+
+    list(){
+        let hostName = localStorage.getItem("hostName");
+        
+        if (hostName) {
+            document.getElementById("btnDelHostName").style.display = "block";
+            document.getElementById("btnAddHostName").style.display = "none";
+            document.getElementById("hostNameInput").value = JSON.parse(hostName);
+        }
+        
+    }
+
+    delete(){
+        localStorage.removeItem("hostName");
+        document.getElementById("btnAddHostName").style.display = "block";
+        document.getElementById("btnDelHostName").style.display = "none";
+
+    }
+}
+
+const objHost = new Host();
+
+document.getElementById("btnAddHostName").onclick = () => {
+    let nameHost = document.getElementById("hostNameInput").value;
+    objHost.create(nameHost);
+    
+};
+
+objHost.list();
+
+
+document.getElementById("btnDelHostName").onclick = () => {
+    document.getElementById("hostNameInput").value = "";
+    objHost.delete();
+};
+
+
+// -----------------------------------------------------------------------------
+
+class Receptionist {
+
+    constructor(){
+        this.receptionistName;
+    }
+
+    create(receptionistName){
+
+        if (receptionistName == "") {
+            alert("Digite um nome.");
+
+        }else{
+
+            localStorage.setItem("receptionistName", JSON.stringify(receptionistName));
+            document.getElementById("btnAddReceptionistName").style.display = "none";
+            document.getElementById("receptionistNameInput").value = "";
+            document.getElementById("btnDelReceptionistName").style.display = "block";
+            window.location.reload();
+        }
+    }
+
+    list(){
+        let receptionistName = localStorage.getItem("receptionistName");
+
+        if (receptionistName) {
+            document.getElementById("receptionistNameInput")
+                                    .value = JSON.parse(receptionistName);
+            document.getElementById("btnDelReceptionistName")
+                                    .style.display = "block";
+            document.getElementById("btnAddReceptionistName")
+                                    .style.display = "none";
+        }
+    }
+
+    delete(){
+        localStorage.removeItem("receptionistName");
+        document.getElementById("btnAddReceptionistName").style.display = "block";
+        document.getElementById("btnDelReceptionistName").style.display = "none";
+    }
+}
+
+const objReceptionist = new Receptionist();
+
+document.getElementById("btnAddReceptionistName").onclick = () => {
+    let nameRecept = document.getElementById("receptionistNameInput").value;
+    objReceptionist.create(nameRecept);
+};
+
+objReceptionist.list();
+
+document.getElementById("btnDelReceptionistName").onclick = () => {
+    document.getElementById("receptionistNameInput").value = "";
+    objReceptionist.delete();
+};
+
+
+// -----------------------------------------------------------------------------
+
+class Guest{
+
+    constructor(){
+        this.guests = [];
+    }
+
+    createList(guestName){
+        
+        if (guestName == "") {
+            alert("Digite um nome.");
+            
+        }else{
+
+        let guestNameInput = document.getElementById("guestNameInput");
+        let elGuestNameInput =  guestNameInput.value;
+        this.guests.push({name: elGuestNameInput});
+        guestNameInput.value = "";
+        
+
+        let elNameList = document.getElementById("guestNameList");
+        elNameList.innerHTML = "";
+        
+            for (const guest of this.guests) {
+
+                let elTr = document.createElement("tr")
+                let elTd1 = document.createElement("td");
+                let elTd2 = document.createElement("td");
+                let elTdAEdit = document.createElement("a");
+                elTdAEdit.setAttribute("href", "#");
+                elTdAEdit.classList.add("btn"); //Adding the class
+                elTdAEdit.classList.add("btn-primary"); //Adding the class
+                let elTdADel = document.createElement("a");
+                elTdADel.setAttribute("href", "#");
+                elTdADel.classList.add("btn"); //Adding the class
+                elTdADel.classList.add("btn-danger"); //Adding the class
+                let elTdEdit = document.createTextNode("Editar");
+                let elTdDel = document.createTextNode("Excluir");
+                let elGuestName = document.createTextNode(guest.name);
+
+                // name of the guest
+                elTd1.appendChild(elGuestName);
+                elTr.appendChild(elTd1)
+                elNameList.appendChild(elTr);
+
+                // edit button
+                elTdAEdit.appendChild(elTdEdit);
+                elTd2.appendChild(elTdAEdit);
+                elTr.appendChild(elTd2);
+                elNameList.appendChild(elTr);
+
+                // delete button
+                elTdADel.appendChild(elTdDel);
+                elTd2.appendChild(elTdADel);
+                elTr.appendChild(elTd2);
+                elNameList.appendChild(elTr);
+            }
+        }    
+    }
+}
+
+const objGuest = new Guest();
+
+
+document.getElementById("btnAddGuest").onclick = () =>{
+    let guestName = document.getElementById("guestNameInput").value;
+    objGuest.createList(guestName);
+}
+
+
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
